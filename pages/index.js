991f2c6c714c659bf486b635a48d467c95f9afc0
@wwwjim <send@jimmy.email>
2020-05-06 14:35:20 -0700

    jim: version testing for debugging

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ const STYLES_H1 = css`
   line-height: 1.1;
   font-weight: 400;
   padding: 24px;
-  text-align: center; 
+  text-align: center;
 `;
 
 const STYLES_H2 = css`
@@ -97,7 +97,7 @@ const STYLES_H3 = css`
   max-width: 768px;
   margin-top: 1.414rem;
   padding: 24px;
-  text-align: center; 
+  text-align: center;
 
   strong {
     font-family: "inter-semi-bold";
@@ -232,6 +232,7 @@ export default class IndexPage extends React.Component {
     const description =
       "The IPFS Pinning Summit is a 2-day virtual conference designed for the infrastructure and service providers of the distributed web.";
     const url = "https://ipfspinningsummit.com";
+    console.log("00.00.01");
 
     return (
       <React.Fragment>
@@ -321,35 +322,34 @@ export default class IndexPage extends React.Component {
             </div>
           </div>
         </div>
-        <div className={STYLES_SECTION} id="schedule">  
-          <H2>May 7th — Schedule</H2> 
+        <div className={STYLES_SECTION} id="schedule">
+          <H2>May 7th — Schedule</H2>
 
-          <Schedule data={Fixtures.ScheduleOne} />  
-        </div>  
+          <Schedule data={Fixtures.ScheduleOne} />
+        </div>
 
-        <div className={STYLES_SECTION}>  
-          <H2>May 8th — Schedule</H2> 
+        <div className={STYLES_SECTION}>
+          <H2>May 8th — Schedule</H2>
 
-          <Schedule data={Fixtures.ScheduleTwo} />  
-        </div>  
+          <Schedule data={Fixtures.ScheduleTwo} />
+        </div>
 
-        <div className={STYLES_SECTION} id="speakers">  
-          <H2>Speakers</H2> 
+        <div className={STYLES_SECTION} id="speakers">
+          <H2>Speakers</H2>
 
-          <Speakers data={Fixtures.Presenters} /> 
-        </div>  
+          <Speakers data={Fixtures.Presenters} />
+        </div>
 
-        <div className={STYLES_SECTION} id="organizations">  
-          <H2>Organizations</H2> 
+        <div className={STYLES_SECTION} id="organizations">
+          <H2>Organizations</H2>
 
-          <Organizations data={Fixtures.Organizations} /> 
+          <Organizations data={Fixtures.Organizations} />
+        </div>
 
-        </div>  
-
-        <div className={STYLES_SECTION} id="watch"> 
-          <H2>Watch</H2>  
-          <H3>Uploaded videos and keynotes will appear here.</H3> 
-        </div>  
+        <div className={STYLES_SECTION} id="watch">
+          <H2>Watch</H2>
+          <H3>Uploaded videos and keynotes will appear here.</H3>
+        </div>
 
         <div className={STYLES_SECTION} id="about">
           <H2>About</H2>
